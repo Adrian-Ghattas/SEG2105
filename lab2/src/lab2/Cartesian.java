eda8f66ed38b893880bf731747212b2a11dae2c4
supremerkk <70713255+supremerkk@users.noreply.github.com>
2023-09-25 12:16:23 -0400

    Update Cartesian.java

--- lab2/src/lab2/Cartesian.java
+++ lab2/src/lab2/Cartesian.java
@@ -1,5 +1,58 @@
 package lab2;
 
-public class Cartesian {
+public class Cartesian extends PointCP {
+
+	public Cartesian(double xOrRho, double yOrTheta) {
+		super(xOrRho, yOrTheta);
+		
+	}
+
+	@Override
+	public double getX() {
+		// TODO Auto-generated method stub
+		return xOrRho;
+	}
+
+	@Override
+	public double getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getRho() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getTheta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void convertStorageToPolar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void convertStorageToCartesian() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PointCP rotatePoint(double rotation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
