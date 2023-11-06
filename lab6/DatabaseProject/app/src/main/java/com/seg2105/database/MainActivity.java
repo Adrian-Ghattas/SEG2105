ca57184eacb79b0ee9558906f5e068f836dc27e6
Michael Massaad <mikmas2004@icloud.com>
2023-11-06 12:08:17 -0500

    Final steps

--- lab6/DatabaseProject/app/src/main/java/com/seg2105/database/MainActivity.java
+++ lab6/DatabaseProject/app/src/main/java/com/seg2105/database/MainActivity.java
@@ -30,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
         Product product = new Product(productBox.getText().toString(), sku);
 
-        // TODO: add to database
+        MyDBHandler dbHandler = new MyDBHandler(this);
+        dbHandler.addProduct(product);
 
         productBox.setText("");
 
@@ -40,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void lookupProduct (View view) {
 
-        // TODO: get from Database
-        Product product = null;
+        MyDBHandler dbHandler = new MyDBHandler(this);
+        Product product = dbHandler.findProduct(productBox.getText().toString());
 
         if (product != null) {
             idView.setText(String.valueOf(product.getID()));
@@ -54,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void removeProduct (View view) {
 
-        // TODO: remove from database
-        boolean result = false;
+        MyDBHandler dbHandler = new MyDBHandler(this);
+        boolean result = dbHandler.deleteProduct(productBox.getText().toString());
 
         if (result) {
             idView.setText("Record Deleted");
